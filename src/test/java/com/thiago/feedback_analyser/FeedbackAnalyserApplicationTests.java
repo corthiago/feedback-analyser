9cf695100f4d8aa117f48b583a33743ea936f250
Thiago Correia <cor.thiago@gmail.com>
2026-08-26 22:43:07 -0300

    chore: initial commit

---
+++ src/test/java/com/thiago/feedback_analyser/FeedbackAnalyserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiago.feedback_analyser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackAnalyserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
