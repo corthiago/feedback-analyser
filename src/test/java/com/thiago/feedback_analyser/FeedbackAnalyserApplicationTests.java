ea91a08b5b01d48f0978f10ae00fdc9e391c9c80
Thiago Correia <cor.thiago@gmail.com>
2026-08-28 19:34:23 -0300

    feat: create unit tests

--- src/test/java/com/thiago/feedback_analyser/FeedbackAnalyserApplicationTests.java
+++ src/test/java/com/thiago/feedback_analyser/FeedbackAnalyserApplicationTests.java
@@ -2,8 +2,10 @@ package com.thiago.feedback_analyser;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = "gemini.api.key=test-key")
 class FeedbackAnalyserApplicationTests {
 
 	@Test
